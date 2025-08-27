82f6211290d8cae1ee47914ac746ae8980e3a545
Avinash <avinashrana55@yahoo.com>
2025-08-27 15:07:49 +0530

    Header component created

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Search from "../Search";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { IoGitCompareOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa";
+import { Tooltip } from "@mui/material";
 
 const Header = () => {
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      right: -3,
+      top: 13,
+      border: `2px solid ${(theme.vars ?? theme).palette.background.paper}`,
+      padding: "0 4px",
+    },
+  }));
+
   return (
     <header>
       <div className="top-strip py-2 border-gray-300 border-t-[1px] border-b-[1px]">
         <div className="container">
           <div className="flex justify-center items-center">
             <div className="w-[50%] col1">
-              <p className="font-[500] text-[12px]">Get upto 50% off for new season styles, limited time only</p>
+              <p className="font-[500] text-[12px]">
+                Get upto 50% off for new season styles, limited time only
+              </p>
             </div>
             <div className="flex justify-end items-center col2">
               <ul className="flex items-center gap-3">
                 <li className="list-none">
-                  <Link to="/help-center" className="font-[500] text-[13px] transition link" >Help Center</Link>
+                  <Link
+                    to="/help-center"
+                    className="font-[500] text-[13px] transition link"
+                  >
+                    Help Center
+                  </Link>
                 </li>
                 <li className="list-none">
-                  <Link to="/order-tracking" className="font-[500] text-[13px] transition link" >Order Tracking</Link>
+                  <Link
+                    to="/order-tracking"
+                    className="font-[500] text-[13px] transition link"
+                  >
+                    Order Tracking
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -26,11 +55,60 @@ const Header = () => {
 
       <div className="py-3 header">
         <div className="flex justify-between items-center container">
-          <div className="w-[30%] col1">
-            <Link to={"/"}><img src="/logo.png" /></Link>
+          <div className="w-[25%] col1">
+            <Link to={"/"}>
+              <img src="/logo.png" />
+            </Link>
           </div>
-          <div className="w-[40%] col2"></div>
-          <div className="w-[30%] col3"></div>
+          <div className="w-[45%] col2">
+            <Search />
+          </div>
+          <div className="flex justify-end items-center pl-7 w-[30%] col3">
+            <ul className="flex items-center gap-3">
+              <li className="list-none">
+                <Link
+                  to={"/login"}
+                  className="font-[500] text-[15px] transition link"
+                >
+                  Login
+                </Link>{" "}
+                |{" "}
+                <Link
+                  to={"/register"}
+                  className="font-[500] text-[15px] transition link"
+                >
+                  Register
+                </Link>
+              </li>
+              <li>
+                <Tooltip title="Compare">
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <IoGitCompareOutline />
+                    </StyledBadge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+              <li>
+                <Tooltip title="Add to cart">
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <MdOutlineShoppingCart />
+                    </StyledBadge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+              <li>
+                <Tooltip title="Wishlist">
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <FaRegHeart />
+                    </StyledBadge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
+import { Button } from "@mui/material";
+import { IoSearch } from "react-icons/io5";
 
 const Search = () => {
   return (
-    <div className='searchBox'>
-      
+    <div className="relative bg-[#e5e5e5] p-2 rounded-[5px] w-[100%] h-[50px] searchBox">
+      <input
+        type="text"
+        placeholder="Search for products..."
+        className="p-2 focus:outline-none w-full h-[35px] text-[15px]"
+      />
+      <Button className="top-[8px] right-[5px] z-50 !absolute !rounded-full !w-[37px] !min-w-[37px] h-[37px] !text-black">
+        <IoSearch className="text-[#4e4e4e] text-[22px]" />
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
