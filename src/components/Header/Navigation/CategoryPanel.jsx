f1040a6ebb95ad6dfddc4798434e0abbb8f9ee37
Avinash <avinashrana55@yahoo.com>
2025-08-27 18:29:17 +0530

    Added Category Panel and implemented in Navbar

---
+++ src/components/Header/Navigation/CategoryPanel.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+const CategoryPanel = (props) => {
+
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={() => props.openCategoryPanel(false)} >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              {/* <ListItemIcon>
+                {index % 2 === 0 ? "<InboxIcon />" : "<MailIcon />"}
+              </ListItemIcon> */}
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              {/* <ListItemIcon>
+                {index % 2 === 0 ? "<InboxIcon />" : "<MailIcon />"}
+              </ListItemIcon> */}
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  return (
+    <div>
+      <Drawer open={props.isOpenCatPanel} onClose={() => props.openCategoryPanel(false)}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+};
+
+export default CategoryPanel;
