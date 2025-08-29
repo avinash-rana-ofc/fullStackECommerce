0cd7efc7573b061b05a076dd0d60629aee74dac6
Avinash <avinashrana55@yahoo.com>
2025-08-29 19:02:27 +0530

    Added slider

---
+++ src/components/HomeSlider/index.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "./style.css";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+
+const HomeSlider = () => {
+  return (
+    <div className="py-4 homeSlider">
+      <div className="container">
+        <Swiper
+          spaceBetween={20}
+          navigation={true}
+          modules={[Navigation]}
+          className="sliderHome"
+        >
+          <SwiperSlide>
+            <div className="rounded-[20px] overflow-hidden item">
+              <img
+                src="https://serviceapi.spicezgold.com/download/1755503364377_1721277298204_banner.jpg"
+                alt="Banner slide"
+                className="w-full"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="rounded-[20px] overflow-hidden item">
+            <img
+              src="https://serviceapi.spicezgold.com/download/1751685144346_NewProject(11).jpg"
+              alt="Banner slide"
+              className="w-full"
+            />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="rounded-[20px] overflow-hidden item">
+            <img
+              src="https://serviceapi.spicezgold.com/download/1751685130717_NewProject(8).jpg"
+              alt="Banner slide"
+              className="w-full"
+            />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="rounded-[20px] overflow-hidden item">
+            <img
+              src="https://serviceapi.spicezgold.com/download/1755503418386_NewProject(12).jpg"
+              alt="Banner slide"
+              className="w-full"
+            />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="rounded-[20px] overflow-hidden item">
+            <img
+              src="https://serviceapi.spicezgold.com/download/1748955932914_NewProject(1).jpg"
+              alt="Banner slide"
+              className="w-full"
+            />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSlider;
